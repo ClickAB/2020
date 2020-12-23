c71ac63a344ed5c70d5684de11cbada632912a8f
201811050958 <956822092@qq.com>
2020-12-23 08:54:20 +0800

    郑志凯个人项目1.0

---
+++ 201811050958郑志凯/zzk1/1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <ostream>
+using namespace std;
+int fun(int a, int b);
+int fun1(int& a, int& b, int& c);
+int fun2(int* a, int* b, int* c);
+int main()
+{
+    int a, b, c;
+    cout << "∵ 分别输入a、b:" << endl;
+    cin >> a >> b;
+    c = fun(a, b);
+    cout << "∴ c = " << c << endl;
+    return 0;
+}
+
+int fun(int a, int b)
+{
+    return (a % 10 * 1000 + b % 10 * 100 + a / 10 * 10 + b / 10);
+}
+
+int fun1(int& a, int& b, int& c)
+{
+    c = a % 10 * 1000 + b % 10 * 100 + a / 10 * 10 + b / 10;
+    return 0;
+}
+
+int fun2(int* a, int* b, int* c)
+{
+    *c = *a % 10 * 1000 + *b % 10 * 100 + *a / 10 * 10 + *b / 10;
+    return 0;
+}
---
+++ 201811050958郑志凯/zzk6/6.cpp
@@ -0,0 +1,77 @@
+﻿#include<iostream>
+#include <math.h>
+using namespace std;
+
+class vehicle
+{
+protected:
+	int wheels;
+	int weight;
+public:
+	vehicle(int wh, int we)
+	{
+		wheels = wh;
+		weight = we;
+	}
+	void disp()
+	{
+		cout << "wheels = " << wheels << ", weight = " << weight << endl;
+	}
+};
+class car : private vehicle
+{
+	int passenger_load;
+public:
+	car(int wh, int we, int pa) : vehicle(wh, we)
+	{
+		passenger_load = pa;
+	}
+	void disp()
+	{
+		vehicle::disp();
+		cout << "passenger_load = " << passenger_load << endl;
+	}
+};
+class truck : private vehicle
+{
+	int passenger_load;
+	int payload;
+public:
+	truck(int wh, int we, int pa, int pay) : vehicle(wh, we)
+	{
+		passenger_load = pa;
+		payload = pay;
+	}
+	void disp()
+	{
+		vehicle::disp();
+		cout << "passenger_load = " << passenger_load << ", payload = " << payload << endl;
+	}
+};
+
+void main()
+{
+	int wh·v, we·v, wh·c, we·c, pa·c, wh·t, we·t, pa·t, pay·t;
+	cout << "请输入vehicle的wheel和weight：" << endl;
+	cin >> wh·v >> we·v;
+	cout << "请输入car的wheel、weight和passenger_load：" << endl;
+	cin >> wh·c >> we·c >> pa·c;
+	cout << "请输入car的wheel、weight、passenger_load和payload：" << endl;
+	cin >> wh·t >> we·t >> pa·t >> pay·t;
+	cout << "\n" << endl;
+
+	cout << "vehicle：" << endl;
+	vehicle v(wh·v, we·v);
+	v.disp();
+	cout << "\n" << endl;
+
+	cout << "car：" << endl;
+	car c(wh·c, we·c, pa·c);
+	c.disp();
+	cout << "\n" << endl;
+
+	cout << "truck：" << endl;
+	truck t(wh·t, we·t, pa·t, pay·t);
+	t.disp();
+	cout << "\n" << endl;
+}
