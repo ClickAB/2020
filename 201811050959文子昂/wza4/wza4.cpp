3155656194067aabcef8a6a46556c0fb61ed47eb
WZANB <2251950359@qq.com>
2020-12-28 08:58:48 +0800

    文子昂个人项目2.0

---
+++ 201811050959文子昂/wza4/wza4.cpp
@@ -0,0 +1,49 @@
+﻿#include "stdio.h"
+void main() {
+	int M, N, c;
+	char a[100][100];
+	int i, j, k;
+	printf("请输入长和宽\n");
+	scanf_s("%d%d", &N, &M);
+	printf("请输入矩阵\n");
+	for (i = 0; i < M; i++) {
+		for (j = 0; j < N + 1; j++) {
+			scanf_s("%c", &a[i][j]);
+		}
+	}
+	printf("请输入周期\n");
+	scanf_s("%d", &c);
+	for (k = 0; k < c; k++)
+	{
+		for (i = 0; i < M; i++)
+		{
+			for (j = 0; j < N + 1; j++)
+			{
+				if (a[i][j] == 'X')
+				{
+					if (a[i + 1][j] == 'O')
+						a[i + 1][j] = 'N';
+					if (a[i - 1][j] == 'O')
+						a[i - 1][j] = 'N';
+					if (a[i][j + 1] == 'O')
+						a[i][j + 1] = 'N';
+					if (a[i][j - 1] == 'O')
+						a[i][j - 1] = 'N';
+				}
+			}
+		}
+		for (i = 0; i < M; i++)
+		{
+			for (j = 0; j < N; j++)
+			{
+				if (a[i][j] == 'N')
+					a[i][j] = 'X';
+			}
+		}
+	}
+	for (i = 0; i < M; i++) {
+		for (j = 0; j < N + 1; j++) {
+			printf("%c", a[i][j]);
+		}printf("\n");
+	}printf("\n");
+}
---
+++ 201811050959文子昂/wza5/wza5.cpp
@@ -0,0 +1,203 @@
+﻿#include <Windows.h>
+#include <iostream>
+#include <vector>
+#include <String>
+#include <stack>
+#include <cstdlib>
+using namespace std;
+int main()
+{
+	string str_in;
+	vector<string> houzhuibiaodashi;
+	stack<char> CStack;
+	string temp;
+	stack <float> NStack;
+	while (1)
+	{
+		houzhuibiaodashi.clear();
+		while (!CStack.empty())	
+		{
+			CStack.pop();
+		}
+		temp = "";
+		while (!NStack.empty())
+		{
+			NStack.pop();
+		}
+		cout << "请输入多项式：";
+		cin >> str_in;
+		for (int i = 0; i < str_in.length(); i++)
+		{
+			if (str_in[i] == '(')		
+			{
+				CStack.push(str_in[i]);				
+			}
+			else if (str_in[i] >= '0' && str_in[i] <= '9')			
+			{
+				temp = str_in[i];
+				while (str_in[i + 1] >= '0' && str_in[i + 1] <= '9' && i < str_in.length())
+				{
+					temp += str_in[++i];
+				}
+				houzhuibiaodashi.push_back(temp);			
+			}
+			else if (
+				(str_in[i] == '-'
+					&& (i > 0 && !(str_in[i - 1] >= '0' && str_in[i - 1] <= '9' && str_in[i + 1] >= '0' && str_in[i + 1] <= '9'))
+					&& (i > 0 && !(str_in[i - 1] == ')' && str_in[i + 1] >= '0' && str_in[i + 1] <= '9'))	
+					&& (i > 0 && !(str_in[i + 1] == ')' && str_in[i - 1] >= '0' && str_in[i - 1] <= '9')))	
+				|| (str_in[i] == '-' && i == 0)										
+				)//
+			{
+				temp = "";
+
+				temp = str_in[i++];
+				while (str_in[i] >= '0' && str_in[i] <= '9' && i < str_in.length())
+				{
+					temp += str_in[i];
+					i++;
+				}
+
+				houzhuibiaodashi.push_back(temp);				
+
+				i--;		
+
+			}
+			else if (str_in[i] == '-' && str_in[i + 1] == '(' && (i == 0))	
+			{
+
+
+				houzhuibiaodashi.push_back("-1");			
+
+
+				while (!CStack.empty() && (CStack.top() == '*' || CStack.top() == '/'))
+				{
+					temp = "";
+					temp = CStack.top();
+					CStack.pop();
+					houzhuibiaodashi.push_back(temp);
+				}
+				CStack.push('*');
+			}
+
+
+
+			else if (str_in[i] == ')')
+			{
+				temp = "";
+				while (!CStack.empty() && CStack.top() != '(')
+				{
+					temp = "";
+					temp = CStack.top();
+					houzhuibiaodashi.push_back(temp);
+					CStack.pop();
+				}
+				CStack.pop();			
+			}
+
+			else if (str_in[i] == '+' || str_in[i] == '-' || str_in[i] == '*' || str_in[i] == '/')
+			{
+				temp = "";
+				if (str_in[i] == '+' || str_in[i] == '-')
+				{
+					while (!CStack.empty() && (CStack.top() == '+' || CStack.top() == '-' || CStack.top() == '*' || CStack.top() == '/'))
+					{
+						temp = "";
+						temp = CStack.top();
+						CStack.pop();
+						houzhuibiaodashi.push_back(temp);
+					}
+					CStack.push(str_in[i]);
+				}
+				else if (str_in[i] == '*' || str_in[i] == '/')
+				{
+					while (!CStack.empty() && (CStack.top() == '*' || CStack.top() == '/'))
+					{
+						temp = "";
+						temp = CStack.top();
+						CStack.pop();
+						houzhuibiaodashi.push_back(temp);
+					}
+					CStack.push(str_in[i]);
+				}
+
+			}
+
+
+
+
+		}
+
+
+		while (!CStack.empty())
+		{
+			temp = CStack.top();
+			CStack.pop();
+			houzhuibiaodashi.push_back(temp);
+		}
+		int i = 0;
+		float num1, num2;
+		while (!houzhuibiaodashi.empty() && i < houzhuibiaodashi.size())
+		{
+			if (houzhuibiaodashi[i] == "+")
+			{
+				num1 = NStack.top();
+				NStack.pop();
+				num2 = NStack.top();
+				NStack.pop();
+				NStack.push(num1 + num2);
+			}
+			else if (houzhuibiaodashi[i] == "-")
+			{
+				if (i == 0)			
+				{
+					char* data = new char[houzhuibiaodashi.size()];
+					for (int t = 1; t < houzhuibiaodashi[i].size(); t++)
+					{
+						data[t] = houzhuibiaodashi[i][t];
+					}
+
+					NStack.push(0 - atoi(data));
+				}
+				else
+				{
+					num1 = NStack.top();
+					NStack.pop();
+					num2 = NStack.top();
+					NStack.pop();
+					NStack.push(num2 - num1);
+				}
+			}
+			else if (houzhuibiaodashi[i] == "*")
+			{
+				num1 = NStack.top();
+				NStack.pop();
+				num2 = NStack.top();
+				NStack.pop();
+				NStack.push(num1 * num2);
+			}
+			else if (houzhuibiaodashi[i] == "/")
+			{
+				num1 = NStack.top();
+				NStack.pop();
+				num2 = NStack.top();
+				NStack.pop();
+				NStack.push(num2 / num1);
+			}
+			else
+			{
+				char* data = new char[houzhuibiaodashi.size()];
+				for (int t = 0; t < houzhuibiaodashi[i].size(); t++)
+				{
+					data[t] = houzhuibiaodashi[i][t];
+				}
+
+				NStack.push(atoi(data));
+			}
+
+			i++;
+		}
+		cout << "等式的值为" << NStack.top() << endl;
+	}
+	system("pause");
+}
